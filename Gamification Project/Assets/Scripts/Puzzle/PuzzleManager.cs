54def9d6724982dfad41d6c3800ada279673b8ca
NaCl586 <73216718+NaCl586@users.noreply.github.com>
2022-06-12 20:17:04 +0700

    minigame 5 done

---
+++ Gamification Project/Assets/Scripts/Puzzle/PuzzleManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class PuzzleManager : MonoBehaviour
+{
+    [Header("Stuff References")]
+    public Text progress;
+    public Text caption;
+    public GameObject[] puzzles;
+
+    private int correctPieces = -1;
+
+    private int rng;
+
+    [Header("UI References")]
+    public GameObject win;
+    public GameObject winWindow;
+    public RawImage winBackground;
+
+    public void OnEnable()
+    {
+        PuzzlePiece.addScore += increaseScore;
+    }
+
+    public void OnDisable()
+    {
+        PuzzlePiece.addScore -= increaseScore;
+    }
+
+    void Start()
+    {
+        increaseScore();
+        rng = Random.Range(0, 3);
+        for(int i = 0; i < puzzles.Length; i++)
+        {
+            if (i == rng) puzzles[i].SetActive(true);
+            else puzzles[i].SetActive(false);
+        }
+        switch (rng)
+        {
+            case 0: caption.text = "KOKI"; break;
+            case 1: caption.text = "POLISI"; break;
+            case 2: caption.text = "GURU"; break;
+        }
+
+        List<GameObject> puzzlePieces = new List<GameObject>();
+        for(int i=0;i<6;i++)
+        {
+            int index = i + 6;
+            puzzlePieces.Add(puzzles[rng].transform.GetChild(index).gameObject);
+        }
+        for(int i = 0; i < 6; i++)
+        {
+            int rand = Random.Range(0, puzzlePieces.Count);
+            puzzlePieces[rand].transform.SetSiblingIndex(i + 6);
+            puzzlePieces.RemoveAt(rand);
+        }
+    }
+
+    public void increaseScore()
+    {
+        progress.text = (correctPieces + 1) + " / 6";
+        correctPieces++;
+        if (correctPieces >= 6) Win();
+    }
+
+    public void Win()
+    {
+        win.SetActive(true);
+
+        winBackground.color = Color.clear;
+        winBackground.DOColor(new Color(0, 0, 0, 0.5f), 0.5f);
+
+        winWindow.transform.localScale = Vector3.zero;
+        winWindow.transform.DOScale(Vector3.one, 0.5f);
+
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Gamification Project/Assets/Scripts/Puzzle/PuzzlePiece.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PuzzlePiece : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
+{
+    private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    public RectTransform correctTransform;
+    public bool isPlacedCorrectly = false;
+
+    private AudioManager am;
+
+    public delegate void scoreCounter();
+    public static scoreCounter addScore;
+
+    public void Awake()
+    {
+        canvas = GameObject.FindGameObjectWithTag("PuzzleCanvas").GetComponent<Canvas>();
+        am = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<AudioManager>();
+
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        rectTransform.anchoredPosition = new Vector2(Random.Range(-200, 200), Random.Range(-100, 200));
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (isPlacedCorrectly) return;
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (isPlacedCorrectly) return;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (isPlacedCorrectly) return;
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(Vector2.Distance(rectTransform.anchoredPosition, correctTransform.anchoredPosition));
+        if(Vector2.Distance(rectTransform.anchoredPosition, correctTransform.anchoredPosition) < 50f &&
+            !isPlacedCorrectly)
+        {
+            gameObject.SetActive(false);
+            correctTransform.gameObject.GetComponent<Image>().color = Color.white;
+            isPlacedCorrectly = true;
+            am.puCorrect();
+            addScore?.Invoke();
+        }
+    }
+}
